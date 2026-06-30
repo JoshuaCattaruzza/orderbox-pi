426f4eeed87a5f80cb9a338a90a87f8eed5dc08c
JoshuaCattaruzza <joshua.cattaruzza@gmail.com>
2026-06-30 20:00:13 +0100

    Add --password-store=basic to Chromium to suppress keyring dialog on boot

--- scripts/install.sh
+++ scripts/install.sh
@@ -93,7 +93,7 @@ cat > "$AUTOSTART_DIR/orderbox-kiosk.desktop" <<EOF
 [Desktop Entry]
 Type=Application
 Name=OrderBox Kiosk
-Exec=chromium --kiosk --noerrdialogs --disable-infobars --no-first-run --window-size=800,480 --force-renderer-accessibility http://localhost:5000
+Exec=chromium --kiosk --noerrdialogs --disable-infobars --no-first-run --window-size=800,480 --force-renderer-accessibility --password-store=basic http://localhost:5000
 Hidden=false
 X-GNOME-Autostart-enabled=true
 EOF
